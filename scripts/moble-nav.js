b35f4ba16c4542c9f3704f1b17e4444c90036123
Sage <sage@Sages-MacBook-Pro.local>
2026-06-04 12:42:25 -0700

    changes

---
+++ scripts/moble-nav.js
@@ -0,0 +1,30 @@
+{/* <script>
+
+fetch('nav.html')
+
+ .then(response => response.text())
+
+ .then(data => {
+
+document.getElementById('nav-placeholder').innerHTML = data;
+
+// Wait until nav.html is loaded, THEN attach toggle behavior */}
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+
+const navbarLinks = document.getElementsByClassName('kira')[0];
+
+if (toggleButton && navbarLinks) {
+
+toggleButton.addEventListener('click', () => {
+
+navbarLinks.classList.toggle('active');
+
+ });
+
+ }
+
+//  });
+
+
+ 
